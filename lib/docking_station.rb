0a7f3214674db23dc08e90377238c740c0bfcd5a
Mark <markjpoole@yahoo.com>
2020-07-09 20:06:25 +0100

    completion of exercises up to 15

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,11 +1,23 @@
 require_relative 'bike'
 
 class DockingStation
-  attr_reader :bike
+  attr_reader :bikes
+  def initialize
+    @bikes = []
+  end
   def release_bike
-    @bike = Bike.new
+    fail 'No bikes available' if empty?
+    @bikes.pop
   end
   def dock(bike)
-    @bike = bike
+    fail 'Docking station full' if full?
+    @bikes << bike
+  end
+  private
+  def full?
+    @bikes.count >= 20
+  end
+  def empty?
+    @bikes.count == 0
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,18 +5,33 @@ describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 
   it 'releases working bikes' do
+    subject.dock(Bike.new)
     bike = subject.release_bike
     expect(bike).to be_working
   end
+
   it 'docks a bike' do
     expect(subject).to respond_to(:dock).with(1).argument
   end
+
   it 'responds to :bike' do
-    expect(subject).to respond_to(:bike)
+    expect(subject).to respond_to(:bikes)
   end
+
   it 'states bike ID' do
     bike = Bike.new
     subject.dock(bike)
-    expect(subject.bike).to eq(bike)
+    expect(subject.bikes[0]).to eq(bike)
   end
+
+  it 'raises an error when release bikes that do not exist' do
+    expect {subject.release_bike}.to raise_error 'No bikes available'
+  end
+
+  it 'raises an error when dock bike if docking station contains a bike' do
+    bike = Bike.new
+    20.times {subject.dock(bike)}
+    expect {subject.dock(bike)}.to raise_error 'Docking station full'
+  end
+
 end
